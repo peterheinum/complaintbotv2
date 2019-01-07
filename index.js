6a5700b71e67f11bcbb131aaf49a7b274e224e95
peter heinum <peter.heinum@gmail.com>
2019-01-07 10:56:47 +0100

    i think complaintbotv1 is now alive again as complaintbotv2

--- index.js
+++ index.js
@@ -1,34 +1,58 @@
 var SlackBot = require('slackbots');
 
 // create a bot
+const envKey = process.env.KEY;
 var bot = new SlackBot({
-    token: '', // Add a bot https://my.slack.com/services/new/bot and put the token 
+    token: envKey, // Add a bot https://my.slack.com/services/new/bot and put the token 
     name: 'complaintbotv2'
 });
 
-bot.on('start', function() {
+bot.on('start', function () {
     // more information about additional params https://api.slack.com/methods/chat.postMessage
     var params = {
         icon_emoji: ':japanese_ogre:'
     };
-    
+    let complaint = getRandomComplaint();
     // define channel, where bot exist. You can adjust it there https://my.slack.com/services 
-    bot.postMessageToChannel('general', "complaint", params);
-    
+    if (complaint != undefined) {
+        bot.postMessageToChannel('general', complaint, params);
+    }
     // define existing username instead of 'user_name'
     //bot.postMessageToUser('user_name', 'meow!', params); 
-    
+
     // If you add a 'slackbot' property, 
     // you will post to another user's slackbot channel instead of a direct message
     //bot.postMessageToUser('user_name', 'meow!', { 'slackbot': true, icon_emoji: ':cat:' }); 
-    
+
     // define private group instead of 'private_group', where bot exist
     //bot.postMessageToGroup('private_group', 'meow!', params); 
 });
 
 
 
-bot.on('message', function(data) {
+bot.on('message', function (data) {
     // all ingoing events https://api.slack.com/rtm
-    console.log(data);
+    //console.log(data);
 });
+
+function getRandomComplaint() {
+    let complaint = wordList[Math.floor(Math.random() * wordList.length)];
+    return complaint;
+}
+
+
+
+wordList = [
+    'Life is definitely not worth it.',
+    'I regret waking up today.',
+    'Come on shitheads do something productive.',
+    'Remember that time I said I thought you were cool? I lied.',
+    'Do you ever wonder what life would be like if you’d gotten enough oxygen at birth?',
+    'Can you die of constipation? I ask because I’m worried about how full of shit you are.',
+    'You’ll never be the man your mom is.',
+    'Earth is full. Go home.',
+    'Your family tree must be a cactus ‘cause you’re all a bunch of pricks.',
+    'I was going to give you a nasty look but I see that you’ve already got one.',
+    'Eat shit die',
+    'Go fuck yourself'
+]
